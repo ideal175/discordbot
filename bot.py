f94e111138a65ee2daf5863057c486cf11010ff8
ideal175 <85906498+ideal175@users.noreply.github.com>
2021-06-14 15:25:15 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,24 +1,21 @@
 import discord
 from discord.ext import commands
+import os
 
 client = commands.Bot(command_prefix="!")
 
 
 @client.event
-async def on_ready(ctx):
-    await ctx.send("ideal made this bot now sub !yt")
-
-@client.event
 async def on_ready():
-    print("bot ready")
+    print("DrkeDrkeDzon is Online")
 
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hello Drke Drke Dzon")
+    await ctx.send("Hello Kidaro")
 
 @client.command()
 async def h(ctx):
-    await ctx.send("hello Im Drke Drke John theese are all commands right now I hope i helped you :),!hello,!whatyouknow,!plsjoke,!yt")
+    await ctx.send("!hello,!whatyouknow,!plsjoke,!yt")
 
 @client.command()
 async def whatyouknow(ctx):
@@ -26,11 +23,11 @@ async def whatyouknow(ctx):
 
 @client.command()
 async def plsjoke(ctx):
-    await ctx.send("9/11,sorry i had to  :rofl: ")
+    await ctx.send("9/11  :rofl: ")
 
 @client.command()
 async def yt(ctx):
     await ctx.send("https://www.youtube.com/channel/UCLN1vLtK5SU1saRlo1pwCLA")
 
 
-client.run('ODU0MDM5NjQ0OTI3Njg4NzYy.YMeIZg.8StXfHr0R5IQ0CCEmQqn2bRXMC0')
+client.run(os.getenv('TOKEN'))
