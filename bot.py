f8b6703ee3c0aed8e3fa105b67423295aa66a993
ideal175 <85906498+ideal175@users.noreply.github.com>
2023-07-09 22:45:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,164 @@
 import discord
 from discord.ext import commands
+from webserver import keep_alive
+import json
+import requests
 import os
+import youtube_dl
+import nacl
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord import TextChannel
+from youtube_dl import YoutubeDL
+from dotenv import load_dotenv
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix=".")
 
 
 @client.event
 async def on_ready():
-    print("DrkeDrkeDzon is Online")
+    print("prefix is .")
+
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please pass in all requirements :rolling_eyes:.')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You dont have all the requirements :angry:")
+
+
+#The below code bans player.
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+#The below code unbans player.
+@client.command()
+@commands.has_permissions(administrator=True)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name,
+                                               member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
 
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello Kidaro")
 
-@client.command()
-async def h(ctx):
-    await ctx.send("!hello,!whatyouknow,!plsjoke,!yt")
-
-@client.command()
-async def whatyouknow(ctx):
-    await ctx.send("BOUT ROLLING DOWN IN THE DEEP!  :rofl: ")
 
 @client.command()
 async def plsjoke(ctx):
-    await ctx.send("9/11  :rofl: ")
+    await ctx.send("im about to spill my brain on the wall. ")
+
 
 @client.command()
 async def yt(ctx):
     await ctx.send("https://www.youtube.com/channel/UCLN1vLtK5SU1saRlo1pwCLA")
 
-@client.command(pass_context = True)
-async def j(ctx):
-    if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
-    else:
-        await ctx.send("you need to be in voice channel")
 
-@client.command(pass_context = True)
+@client.command()
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+
+# command to play sound from a youtube URL
+@client.command()
+async def play(ctx, url):
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {
+        'before_options':
+        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn'
+    }
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['url']
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.is_playing()
+        await ctx.send('Bot is playing')
+
+# check if the bot is already playing
+    else:
+        await ctx.send("Bot is already playing")
+        return
+
+
+# command to resume voice if it is paused
+@client.command()
+async def resume(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        voice.resume()
+        await ctx.send('Bot is resuming')
+
+
+# command to pause voice if it is playing
+@client.command()
+async def pause(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.pause()
+        await ctx.send('Bot has been paused')
+
+
+# command to stop voice
+@client.command()
+async def stop(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.stop()
+        await ctx.send('Stopping...')
+
+
+# command to clear channel messages
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send("5 Messages have been cleared")
+
+
+@client.command()
+async def clear_all(ctx, amount=900):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send("All Messages have been cleared")
+
+
+@client.command(pass_context=True)
 async def leave(ctx):
     if (ctx.voice_client):
-        await ctx.guild.voice-client.disconnect()
-        await ctx.send("I Left the voice channel")
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I left the voice channel")
     else:
-        await ctx.send("Im not in voice channel")
+        await ctx.send("I am not in voice channel")
 
-client.run(os.getenv('TOKEN'))
+
+keep_alive()
+TOKEN = os.environ.get('DISCORD_BOT_SECRET')
+client.run(TOKEN)
